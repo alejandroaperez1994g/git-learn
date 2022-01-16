474c16801bac1036d523342d064b4b81667ab6bc
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-01-16 03:19:10 +0000

    Test

--- index-ts.js
+++ index-ts.js
@@ -4,12 +4,28 @@
 // -boolean
 // -object
 var person = {
-    name: 'John',
-    age: 36,
-    hobbies: ['walk', 'run', 'futbol']
+  name: "John",
+  age: 36,
+  hobbies: ["walk", "run", "futbol"],
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobbie = _a[_i];
-    console.log(hobbie.toUpperCase());
+  var hobbie = _a[_i];
+  console.log(hobbie.toUpperCase());
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+let personNew = {
+  name: "Ale",
+  age: 28,
+  country: "Spain",
+};
+
+for (let i = 0; i <= arr.length; i++) {
+  try {
+    console.log(arr[i]);
+  } catch (e) {
+    console.log(e);
+  }
 }
