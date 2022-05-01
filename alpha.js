6727bf608ed12741d8dc2dc57ff57eabce7f60be
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-01 22:19:55 +0100

    change some stuf

--- alpha.js
+++
@@ -1,1 +0,0 @@
-console.log("esto es una prueba")
--- autentication.js
+++ autentication.js
@@ -1,6 +1,31 @@
-const testeo2 = () => {
-  console.log("testeo2");
+const list = document.querySelector(".list");
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     data.forEach((element) => {
+//       let liE = document.createElement("li");
+//       liE.textContent = element.title;
+//       list.appendChild(liE);
+//     });
+//   });
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  setList(data);
+}
+
+const setList = (data) => {
+  data.forEach((element) => {
+    let elementList = document.createElement("li");
+    elementList.innerHTML = element.title;
+    list.appendChild(elementList);
+    console.log(elementList);
+  });
 };
-const testeto3 = () => {
-  console.log("testeto3");
-};
+
+fetchData();
